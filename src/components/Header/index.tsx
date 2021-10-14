5e093087db50887fe9d1a66b39472c07703f9aa9
Diego Marin <48814830+diegobnx@users.noreply.github.com>
2021-10-13 23:23:47 -0300

    Projeto ReactJS multi-formulários

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './styles';
+
+export const Header = () => {
+    return (
+        <C.Container>
+            <h1>Cadastro de Desenvolvedor</h1>
+            <p>Cadastre-se agora e fique na lista de espera!</p>
+        </C.Container>
+    );
+}
---
+++ src/components/Theme/index.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+import * as C from "./styles";
+import { Header } from "../Header";
+import { SidebarItem } from "../SidebarItem";
+import { useForm } from "../../contexts/FormContext";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const Theme = ({ children }: Props) => {
+  const { state } = useForm();
+
+  return (
+    <C.Container>
+      <C.Area>
+        <Header />
+
+        <C.Steps>
+          <C.Sidebar>
+            <SidebarItem
+              title="Pessoal"
+              description="Se identifique"
+              icon="profile"
+              path="/"
+              active={state.currentStep === 1}
+            />
+            <SidebarItem
+              title="Profissional"
+              description="Seu nivel"
+              icon="book"
+              path="/step2"
+              active={state.currentStep === 2}
+            />
+            <SidebarItem
+              title="Contatos"
+              description="Seu contato"
+              icon="mail"
+              path="/step3"
+              active={state.currentStep === 3}
+            />
+          </C.Sidebar>
+          <C.Page>{children}</C.Page>
+        </C.Steps>
+      </C.Area>
+    </C.Container>
+  );
+};
